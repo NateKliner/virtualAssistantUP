15e5308d7aaa2039ee05b0e53da26bc3b832a77c
Alex <ahexorange88@gmail.com>
2021-10-28 16:42:23 -0700

    HREF FIX

--- IRVA Capstone/public/javascripts/lists.js
+++ IRVA Capstone/public/javascripts/lists.js
@@ -2,36 +2,36 @@ questions = [
     { //Data Requests
         str: 'What do you want to know about data Requests?',
         choices: ['I received data but it is innacurate or differs from data on the IR website', 'I want to know more about giving and recieving data requests', 'Can I request data?'],
-        correct: 0
+        
     },
     { //I received data but it is innacurate or differs from data on the IR website 
         str: 'You may want to contact IR.',
         choices: ['How soon can I hear back?', 'How do i contact IR?'],
-        correct: 1
+        
     },
     { //Main
         str: 'How can I help you today?',
         choices: ['I would like to know about Institutional Data.', 'I would like to know about Data Access', 'I would like to know about External Resources', 'I would like to know about IR', 'I would like to know about Data Governance', 'I would like to know about Data Protection'],
-        correct: 2
+        
     },
     { //Data Access
         str: 'What would you like to know about Data access?',
         choices: ['I want to access Tableau Dashboards', 'I want to access grant proposals and reports','I have questions about Data Requests'],
-        correct: 1
+        
     },
     { //External Resources
         str: 'What would you like to know about External Resources?',
         choices: ['I have questions about IPEDS', 'I have questions about college navigator.','I have questions about benchmarking data'],
-        correct: 1
+        
     },
     {//Insatitutional Data
         str: 'What would you like to know about institutional Data?',
         choices: ['What is CDS and what data is in it?', 'I have questions about student data.','I have questions about Data Requests','Can I suggest data?', 'I have questions about Tableau Dashboards.'],
-        correct: 1
+        
     },
     { //Data Governance
         str: 'What would you like about data governance?',
         choices: ['What is data governance?', 'What are data definitions?'],
-        correct: 1
+        
     },
 ]
